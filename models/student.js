d42fbc1a5d5a9ffaf959dc5293940ed406a08132
Fourthhy <miguelmanabo4@gmail.com>
2025-09-18 22:17:05 +0800

    Edidted Student Schema

--- models/student.js
+++ models/student.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
   studentID: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  course: { type: String, required: true },
-  mealEligibilityStatus: { type: String, default: 'Eligible' },
-  creditValue: { type: Number, default: 60 } // Added creditValue with a default of 60
+  first_name: { type: String, required: true },
+  middle_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  section: { type: String }, //for basic education
+  program: { type: String }, //for higher education
+  year: { type: String, required: true },
+  mealEligibilityStatus: { type: String, default: 'Ineligible' },
+  creditValue: { type: Number, default: 0 }
 });
-
-module.exports = mongoose.model('Student', studentSchema);
