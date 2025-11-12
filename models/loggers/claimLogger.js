1b449e7c1135f467cde5ed8671843b22908c356a
Fourthhy <miguelmanabo4@gmail.com>
2025-11-13 03:01:53 +0800

    Configured claimLoggger

--- models/loggers/claimLogger.js
+++ models/loggers/claimLogger.js
@@ -4,6 +4,7 @@ const loggerSchema = new mongoose.Schema({
     timeStamp: { type: Date, default: Date.now, required: true },
     studentID: { type: String, required: true },
     action: { type: String, required: true, enum: ['CLAIM-FREE-MEAL', 'CLAIM-FOOD-ITEM', 'CLAIM-ATTEMPT-WAIVED', 'CLAIM-ATTEMPT-CLAIMED', 'CLAIM-ATTEMPT-INELIGIBLE', 'CLAIM-ATTEMPT-INSUFFICIENT-BALANCE', 'CLAIM-ATTEMPT-NO-BALANCE', 'REMOVED-UNUSED-BALANCE', 'ASSIGN-CREDIT'], },
+    overclaim: { type: Boolean,  required: true }, //this determines if the claim exceeds from the assigned value
     creditTaken: { type: Number, required: true }
 })
 
